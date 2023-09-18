ac2ef07d5466e2970fad8b232032f61626602ca9
Will Eastcott <will@playcanvas.com>
2023-09-18 09:39:51 +0100

    Add pc-scene element

--- src/application.mjs
+++ src/application.mjs
@@ -1,26 +1,27 @@
 import { Application, FILLMODE_FILL_WINDOW, RESOLUTION_AUTO } from 'playcanvas';
 
 class ApplicationElement extends HTMLElement {
-    async connectedCallback() {
+    connectedCallback() {
         const shadowRoot = this.attachShadow({ mode: 'open' });
         const canvas = document.createElement('canvas');
         shadowRoot.appendChild(canvas);
-
-        const app = new Application(canvas);
-        app.setCanvasFillMode(FILLMODE_FILL_WINDOW);
-        app.setCanvasResolution(RESOLUTION_AUTO);
-        app.start();
-
-        // Wait for pc-entity to be fully defined
-        await customElements.whenDefined('pc-entity');
-
-        // By now, pc-entity's connectedCallback should have executed
-        const entityElements = this.querySelectorAll('pc-entity');
-        entityElements.forEach(entityElement => {
-            if (entityElement.entity) {
-                app.root.addChild(entityElement.entity);
-            }
+    
+        this.app = new Application(canvas);
+        this.app.setCanvasFillMode(FILLMODE_FILL_WINDOW);
+        this.app.setCanvasResolution(RESOLUTION_AUTO);
+        this.app.start();
+    
+        customElements.whenDefined('pc-entity').then(() => {
+            // By now, pc-entity's connectedCallback should have executed
+            const entityElements = this.querySelectorAll('pc-entity');
+            entityElements.forEach(entityElement => {
+                if (entityElement.entity) {
+                    this.app.root.addChild(entityElement.entity);
+                }
+            });
         });
+
+        this.dispatchEvent(new Event('appInitialized'));
     }
 }
 
--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,16 @@
 import { ApplicationElement } from './application.mjs';
+import { EntityElement } from "./entity.mjs";
+import { SceneElement } from './scene.mjs';
+
 import { CameraComponentElement } from './camera-component.mjs';
 import { LightComponentElement } from './light-component.mjs';
 import { RenderComponentElement } from './render-component.mjs';
-import { EntityElement } from "./entity.mjs";
 
-customElements.define('pc-application', ApplicationElement);
-customElements.define('pc-entity', EntityElement);
-customElements.define('pc-camera-component', CameraComponentElement);
-customElements.define('pc-light-component', LightComponentElement);
-customElements.define('pc-render-component', RenderComponentElement);
+document.addEventListener("DOMContentLoaded", () => {
+    customElements.define('pc-application', ApplicationElement);
+    customElements.define('pc-entity', EntityElement);
+    customElements.define('pc-camera-component', CameraComponentElement);
+    customElements.define('pc-light-component', LightComponentElement);
+    customElements.define('pc-render-component', RenderComponentElement);
+    customElements.define('pc-scene', SceneElement);
+});
