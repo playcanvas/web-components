07883a979e424b687bc28129674c9cb39eef0be4
Will Eastcott <will@playcanvas.com>
2023-09-24 20:35:28 +0100

    Add model element (#11)

---
+++ src/asset.mjs
@@ -0,0 +1,70 @@
+class AssetElement extends HTMLElement {
+    static observedAttributes = ['src', 'id'];
+
+    constructor() {
+        super();
+        this._src = '';
+        this._id = '';
+        this._type = '';
+    }
+
+    connectedCallback() {
+        this._src = this.getAttribute('src') || '';
+        this._id = this.getAttribute('id') || '';
+        this._determineType();
+        this._loadAsset();
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'src' && this._src !== newValue) {
+            this._src = newValue;
+            this._determineType();
+            this._loadAsset();
+        }
+        if (name === 'id' && this._id !== newValue) {
+            this._id = newValue;
+        }
+    }
+
+    _determineType() {
+        if (this._src.endsWith('.glb')) {
+            this._type = 'model';
+        } else if (
+            this._src.endsWith('.png') ||
+        this._src.endsWith('.jpg') ||
+        this._src.endsWith('.jpeg') ||
+        this._src.endsWith('.gif')
+        ) {
+            this._type = 'texture';
+        } else {
+            this._type = '';
+            console.error('Unsupported asset type for src:', this._src);
+        }
+    }
+
+    _loadAsset() {
+      // Implement asset loading logic here.
+      // This might involve adding the asset to a global asset registry,
+      // triggering the asset loading in your PlayCanvas app, etc.
+      // e.g.,
+      // if (this._type === 'texture') {
+      //   loadTexture(this._src, this._id);
+      // } else if (this._type === 'model') {
+      //   loadModel(this._src, this._id);
+      // }
+    }
+
+    get src() {
+        return this._src;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get type() {
+        return this._type;
+    }
+}
+
+export { AssetElement };
--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,7 @@
 import { ApplicationElement } from './application.mjs';
+import { AssetElement } from './asset.mjs';
 import { EntityElement } from "./entity.mjs";
+import { ModelElement } from './model.mjs';
 import { SceneElement } from './scene.mjs';
 
 import { CameraComponentElement } from './camera-component.mjs';
@@ -8,7 +10,9 @@ import { RenderComponentElement } from './render-component.mjs';
 
 document.addEventListener("DOMContentLoaded", () => {
     customElements.define('pc-app', ApplicationElement);
+    customElements.define('pc-asset', AssetElement);
     customElements.define('pc-entity', EntityElement);
+    customElements.define('pc-model', ModelElement);
     customElements.define('pc-camera', CameraComponentElement);
     customElements.define('pc-light', LightComponentElement);
     customElements.define('pc-render', RenderComponentElement);
