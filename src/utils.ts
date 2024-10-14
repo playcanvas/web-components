8419d338afb402a1f2cdb2e9928b39e10361a6e6
Will Eastcott <will@playcanvas.com>
2024-10-14 14:35:31 +0100

    Add tags to EntityElement and refactor class

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,11 @@
-import { Color } from 'playcanvas';
+import { Color, Quat, Vec3 } from 'playcanvas';
 
 /**
  * Parse a color string into a Color object. String can be in the format of '#rgb', '#rgba',
  * '#rrggbb', '#rrggbbaa', or a string of 3 or 4 comma-delimited numbers.
  *
- * @param {string} value - The color string to parse
- * @returns {Color} The parsed Color object
+ * @param value - The color string to parse.
+ * @returns The parsed Color object.
  */
 export const parseColor = (value: string): Color => {
     if (value.startsWith('#')) {
@@ -14,4 +14,9 @@ export const parseColor = (value: string): Color => {
 
     const components = value.split(',').map(Number);
     return new Color(components);
+};
+
+export const parseVec3 = (value: string): Vec3 => {
+    const components = value.split(',').map(Number);
+    return new Vec3(components);
 };
