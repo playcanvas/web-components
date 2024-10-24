714101ec9e24d0134947b98c5ed1fbfd79b54bef
Will Eastcott <will@playcanvas.com>
2024-10-24 21:17:28 +0100

    Switch sky element to use pc-asset (#31)

--- src/sky.ts
+++ src/sky.ts
@@ -1,14 +1,13 @@
-import { Asset, Quat } from 'playcanvas';
+import { Quat } from 'playcanvas';
 
 import { AppElement } from './app';
+import { AssetElement } from './asset';
 
 /**
  * Represents a sky in the PlayCanvas engine.
  */
 class SkyElement extends HTMLElement {
-    static observedAttributes = ['src', 'intensity', 'rotation', 'level'];
-
-    private _src = '';
+    private _asset = '';
 
     private _intensity = 1;
 
@@ -17,56 +16,97 @@ class SkyElement extends HTMLElement {
     private _level = 0;
 
     connectedCallback() {
-        this._updateSkybox();
+    }
+
+    getAsset() {
+        const assetElement = document.querySelector(`pc-asset[id="${this._asset}"]`) as AssetElement;
+        return assetElement!.asset;
+    }
+
+    getScene() {
+        const el = this.closest('pc-app') as AppElement | null;
+        if (!el) {
+            return;
+        }
+        return el.app!.scene;
+    }
+
+    static get observedAttributes() {
+        return ['asset', 'intensity', 'level', 'rotation'];
+    }
+
+    set asset(value: string) {
+        this._asset = value;
+        const scene = this.getScene();
+        if (scene) {
+            const asset = this.getAsset();
+            if (asset) {
+                if (asset.resource) {
+                    scene.envAtlas = asset.resource;
+                } else {
+                    asset.once('load', () => {
+                        scene.envAtlas = asset.resource;
+                    });
+                }
+            }
+        }
+    }
+
+    get asset() {
+        return this._asset;
+    }
+
+    set intensity(value: number) {
+        this._intensity = value;
+        const scene = this.getScene();
+        if (scene) {
+            scene.skyboxIntensity = this._intensity;
+        }
+    }
+
+    get intensity() {
+        return this._intensity;
+    }
+
+    set rotation(value: number[]) {
+        this._rotation = value;
+        const scene = this.getScene();
+        if (scene) {
+            scene.skyboxRotation = new Quat().setFromEulerAngles(this._rotation[0], this._rotation[1], this._rotation[2]);
+        }
+    }
+
+    get rotation() {
+        return this._rotation;
+    }
+
+    set level(value: number) {
+        this._level = value;
+        const scene = this.getScene();
+        if (scene) {
+            scene.skyboxMip = this._level;
+        }
+    }
+
+    get level() {
+        return this._level;
     }
 
     attributeChangedCallback(name: string, _oldValue: string, newValue: string) {
         switch (name) {
-            case 'src':
-                this._src = newValue;
+            case 'asset':
+                this.asset = newValue;
                 break;
             case 'intensity':
-                this._intensity = parseFloat(newValue);
+                this.intensity = parseFloat(newValue);
                 break;
             case 'rotation':
-                this._rotation = newValue.split(',').map(Number);
+                this.rotation = newValue.split(',').map(Number);
                 break;
             case 'level':
-                this._level = parseInt(newValue, 10);
+                this.level = parseInt(newValue, 10);
                 break;
         }
-        this._updateSkybox();
-    }
-
-    _updateSkybox() {
-        const el = this.closest('pc-app') as AppElement | null;
-        if (!el) {
-            return;
-        }
-        const scene = el.app!.scene;
-        this._loadTexture();
-        scene.skyboxIntensity = this._intensity;
-        scene.skyboxRotation = new Quat().setFromEulerAngles(this._rotation[0], this._rotation[1], this._rotation[2]);
-        scene.skyboxMip = this._level;
-    }
-
-    _loadTexture() {
-        const el = this.closest('pc-app') as AppElement | null;
-        if (!el) {
-            return;
-        }
-        const asset = new Asset(this._src, 'texture', {
-            url: this._src
-        });
-        asset.once('load', (asset) => {
-            el.app!.scene.envAtlas = asset.resource;
-        });
-        asset.once('error', (err) => {
-            console.error(err);
-        });
-
-        el.app!.assets.add(asset);
-        el.app!.assets.load(asset);
     }
 }
 
