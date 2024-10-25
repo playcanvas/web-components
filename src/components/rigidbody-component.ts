473c328340ef22e9994768101422a1abbf5ce797
Will Eastcott <will@playcanvas.com>
2024-10-25 15:39:59 +0100

    Add more rigidbody attributes

--- src/components/rigidbody-component.ts
+++ src/components/rigidbody-component.ts
@@ -8,6 +8,24 @@ import { ComponentElement } from './component';
  * @category Components
  */
 class RigidbodyComponentElement extends ComponentElement {
+    /**
+     * The friction of the rigidbody.
+     */
+    private _friction: number = 0.5;
+
+    /**
+     * The mass of the rigidbody.
+     */
+    private _mass: number = 1;
+
+    /**
+     * The restitution of the rigidbody.
+     */
+    private _restitution: number = 0;
+
+    /**
+     * The type of the rigidbody.
+     */
     private _type: string = 'static';
 
     /**
@@ -19,6 +37,9 @@ class RigidbodyComponentElement extends ComponentElement {
 
     getInitialComponentData() {
         return {
+            friction: this._friction,
+            mass: this._mass,
+            restitution: this._restitution,
             type: this._type
         };
     }
@@ -29,6 +50,39 @@ class RigidbodyComponentElement extends ComponentElement {
      */
     get component(): RigidBodyComponent | null {
         return super.component as RigidBodyComponent | null;
+    }
+
+    set friction(value: number) {
+        this._friction = value;
+        if (this.component) {
+            this.component.friction = value;
+        }
+    }
+
+    get friction() {
+        return this._friction;
+    }
+
+    set mass(value: number) {
+        this._mass = value;
+        if (this.component) {
+            this.component.mass = value;
+        }
+    }
+
+    get mass() {
+        return this._mass;
+    }
+
+    set restitution(value: number) {
+        this._restitution = value;
+        if (this.component) {
+            this.component.restitution = value;
+        }
+    }
+
+    get restitution() {
+        return this._restitution;
     }
 
     set type(value: string) {
@@ -43,13 +97,22 @@ class RigidbodyComponentElement extends ComponentElement {
     }
 
     static get observedAttributes() {
-        return [...super.observedAttributes, 'type'];
+        return [...super.observedAttributes, 'friction', 'mass', 'restitution', 'type'];
     }
 
     attributeChangedCallback(name: string, _oldValue: string, newValue: string) {
         super.attributeChangedCallback(name, _oldValue, newValue);
 
         switch (name) {
+            case 'friction':
+                this.friction = parseFloat(newValue);
+                break;
+            case 'mass':
+                this.mass = parseFloat(newValue);
+                break;
+            case 'restitution':
+                this.restitution = parseFloat(newValue);
+                break;
             case 'type':
                 this.type = newValue;
                 break;
