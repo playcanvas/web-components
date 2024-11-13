a489a2a523a00ecd27279f837d0de77af455488a
Will Eastcott <will@playcanvas.com>
2024-11-13 10:33:28 +0000

    Simplify VR integration

--- examples/scripts/vr.mjs
+++ examples/scripts/vr.mjs
@@ -3,8 +3,12 @@ document.addEventListener('DOMContentLoaded', async () => {
     const camera = await document.querySelector('pc-camera').ready();
 
     if (camera.xrAvailable) {
-        const button = document.getElementById('xr-button');
-        button.style.display = 'block';
+        const button = document.createElement('button');
+        button.textContent = 'Enter VR';
+        button.style.position = 'absolute';
+        button.style.bottom = '20px';
+        button.style.right = '20px';
+        document.body.appendChild(button);
 
         button.addEventListener('click', () => {
             camera.startXr('immersive-vr', 'local-floor');
