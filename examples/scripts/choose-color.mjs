87b4b16a9dfd7037c7dc3778c85db6a238b4d2f0
Will Eastcott <will@playcanvas.com>
2024-11-17 23:45:15 +0000

    Make examples fill screen on iOS

--- examples/scripts/choose-color.mjs
+++ examples/scripts/choose-color.mjs
@@ -66,8 +66,8 @@ export class ChooseColor extends Script {
         const container = document.createElement('div');
         Object.assign(container.style, {
             position: 'absolute',
-            top: '10px',
-            left: '10px',
+            top: 'max(10px, env(safe-area-inset-top))',
+            left: 'max(10px, env(safe-area-inset-left))',
             zIndex: '1000',
             display: 'grid',
             gridTemplateColumns: 'repeat(5, 1fr)',
