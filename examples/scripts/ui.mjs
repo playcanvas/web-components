c2f0d4cbed90a0b8b88ff67fa394f33373331223
Will Eastcott <will@playcanvas.com>
2024-11-19 19:36:10 +0000

    Tweak JS for fullscreen

--- examples/scripts/ui.mjs
+++ examples/scripts/ui.mjs
@@ -67,7 +67,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
 
     // Add fullscreen button if supported
-    if (document.documentElement.requestFullscreen && !(/iPhone|iPod/.test(navigator.platform))) {
+    if (document.documentElement.requestFullscreen && document.exitFullscreen) {
         const fullscreenButton = createButton({
             icon: `<svg width="32" height="32" viewBox="0 0 24 24">
                 <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z" fill="currentColor"/>
