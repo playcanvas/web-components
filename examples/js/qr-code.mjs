da420dba5c858bea281657449969fcdc214c587b
Will Eastcott <will@playcanvas.com>
2024-11-19 20:14:10 +0000

    Fix QR code panel

--- examples/js/qr-code.mjs
+++ examples/js/qr-code.mjs
@@ -7,5 +7,16 @@ export function showQRCode(path) {
     const modal = document.getElementById('qr-modal');
     const qrDiv = document.getElementById('qr-code');
     qrDiv.innerHTML = qr.createImgTag(4);
+    
+    // Add click handler to close on backdrop click
+    modal.addEventListener('click', (e) => {
+        const rect = modal.getBoundingClientRect();
+        const isInDialog = (rect.top <= e.clientY && e.clientY <= rect.top + rect.height &&
+            rect.left <= e.clientX && e.clientX <= rect.left + rect.width);
+        if (!isInDialog) {
+            modal.close();
+        }
+    });
+
     modal.showModal();
 }
