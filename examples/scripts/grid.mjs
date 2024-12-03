b70ae32394acccf9c6455e98232364150ed56547
Will Eastcott <will@playcanvas.com>
2024-12-03 14:38:51 +0000

    Restore grid

--- examples/scripts/grid.mjs
+++ examples/scripts/grid.mjs
@@ -1,60 +1,43 @@
+/* eslint-disable-next-line import/no-unresolved */
 import {
-    CULLFACE_NONE,
+    ShaderMaterial,
     SEMANTIC_POSITION,
-    BlendState,
-    DepthState,
-    Mat4,
-    QuadRender,
+    SEMANTIC_TEXCOORD0,
+    BLEND_NORMAL,
+    CULLFACE_NONE,
+    PlaneGeometry,
+    Mesh,
+    MeshInstance,
+    Color,
     Script,
-    createShaderFromCode,
-    BLENDMODE_ONE,
-    BLENDMODE_ONE_MINUS_SRC_ALPHA,
-    BLENDMODE_SRC_ALPHA,
-    BLENDEQUATION_ADD
+    Vec2
 } from 'playcanvas';
 
-// import { Element, ElementType } from './element';
-// import { Serializer } from './serializer';
+const tmpVa = new Vec2();
 
-const vsCode = /* glsl */ `
-    uniform mat4 camera_matrix;
-    uniform vec2 camera_params;
+const EPISILON = 1e-3;
 
-    attribute vec2 vertex_position;
+const vertexCode = /* glsl */ `
+    attribute vec3 vertex_position;
+    attribute vec2 aUv0;
 
-    varying vec3 worldFar;
+    uniform mat4 matrix_model;
+    uniform mat4 matrix_viewProjection;
+
+    varying vec2 uv0;
 
     void main(void) {
-        gl_Position = vec4(vertex_position, 0.0, 1.0);
-
-        vec4 v = camera_matrix * vec4(vertex_position * camera_params, -1.0, 1.0);
-
-        worldFar = v.xyz;
+        gl_Position = matrix_viewProjection * matrix_model * vec4(vertex_position, 1.0);
+        uv0 = aUv0;
     }
 `;
 
-const fsCode = /* glsl */ `
-    uniform vec3 camera_position;
-    uniform mat4 camera_viewProjection;
-    uniform sampler2D blueNoiseTex32;
+const fragmentCode = /* glsl */ `
+    uniform vec2 uHalfExtents;
+    uniform vec3 uColorX;
+    uniform vec3 uColorZ;
 
-    varying vec3 worldFar;
-
-    bool intersectPlane(inout float t, vec3 pos, vec3 dir, vec4 plane) {
-        float d = dot(dir, plane.xyz);
-        if (abs(d) < 1e-06) {
-            return false;
-        }
-
-        float n = -(dot(pos, plane.xyz) + plane.w) / d;
-        if (n < 0.0) {
-            return false;
-        }
-
-        t = n;
-
-        return true;
-    }
+    varying vec2 uv0;
 
     // https://bgolus.medium.com/the-best-darn-grid-shader-yet-727f9278b9d8#1e7c
     float pristineGrid(in vec2 uv, in vec2 ddx, in vec2 ddy, vec2 lineWidth) {
@@ -79,84 +62,52 @@ const fsCode = /* glsl */ `
         return mix(grid2.x, 1.0, grid2.y);
     }
 
-    float calcDepth(vec3 p) {
-        vec4 v = camera_viewProjection * vec4(p, 1.0);
-        return (v.z / v.w) * 0.5 + 0.5;
-    }
-
-    bool writeDepth(float alpha) {
-        vec2 uv = fract(gl_FragCoord.xy / 32.0);
-        float noise = texture2DLodEXT(blueNoiseTex32, uv, 0.0).y;
-        return alpha > noise;
-    }
-
     void main(void) {
-        vec3 p = camera_position;
-        vec3 v = normalize(worldFar - camera_position);
+        vec2 uv = uv0;
 
-        // intersect ray with the world xz plane
-        float t;
-        if (!intersectPlane(t, p, v, vec4(0, 1, 0, 0))) {
-            discard;
-        }
-
-        // calculate grid intersection
-        vec3 pos = p + v * t;
-        vec2 ddx = dFdx(pos.xz);
-        vec2 ddy = dFdy(pos.xz);
+        vec2 pos = (uv * 2.0 - 1.0) * uHalfExtents;
+        vec2 ddx = dFdx(pos);
+        vec2 ddy = dFdy(pos);
 
         float epsilon = 1.0 / 255.0;
 
-        // calculate fade
-        float fade = 1.0 - smoothstep(400.0, 1000.0, length(pos - camera_position));
-        if (fade < epsilon) {
-            discard;
-        }
-
-        vec3 levelPos;
+        vec2 levelPos;
         float levelSize;
         float levelAlpha;
 
-        // 10m grid with colored main axes
         levelPos = pos * 0.1;
         levelSize = 2.0 / 1000.0;
-        levelAlpha = pristineGrid(levelPos.xz, ddx * 0.1, ddy * 0.1, vec2(levelSize)) * fade;
+        levelAlpha = pristineGrid(levelPos, ddx * 0.1, ddy * 0.1, vec2(levelSize));
         if (levelAlpha > epsilon) {
             vec3 color;
-            vec2 loc = max(vec2(0.0), abs(levelPos.xz) - abs(ddx * 0.1) - abs(ddy * 0.1));
-            if (loc.x < levelSize) {
-                if (loc.y < levelSize) {
+            if (abs(levelPos.x) < levelSize) {
+                if (abs(levelPos.y) < levelSize) {
                     color = vec3(1.0);
                 } else {
-                    color = vec3(0.2, 0.2, 1.0);
+                    color = uColorZ;
                 }
-            } else if (loc.y < levelSize) {
-                color = vec3(1.0, 0.2, 0.2);
+            } else if (abs(levelPos.y) < levelSize) {
+                color = uColorX;
             } else {
                 color = vec3(0.9);
             }
             gl_FragColor = vec4(color, levelAlpha);
-            gl_FragDepth = writeDepth(levelAlpha) ? calcDepth(pos) : 1.0;
             return;
         }
 
-        // 1m grid
         levelPos = pos;
         levelSize = 1.0 / 100.0;
-        levelAlpha = pristineGrid(levelPos.xz, ddx, ddy, vec2(levelSize)) * fade;
+        levelAlpha = pristineGrid(levelPos, ddx, ddy, vec2(levelSize));
         if (levelAlpha > epsilon) {
             gl_FragColor = vec4(vec3(0.7), levelAlpha);
-            gl_FragDepth = writeDepth(levelAlpha) ? calcDepth(pos) : 1.0;
             return;
         }
 
-        // 0.1m grid
         levelPos = pos * 10.0;
         levelSize = 1.0 / 100.0;
-        levelAlpha = pristineGrid(levelPos.xz, ddx * 10.0, ddy * 10.0, vec2(levelSize)) * fade;
+        levelAlpha = pristineGrid(levelPos, ddx * 10.0, ddy * 10.0, vec2(levelSize));
         if (levelAlpha > epsilon) {
             gl_FragColor = vec4(vec3(0.7), levelAlpha);
-            gl_FragDepth = writeDepth(levelAlpha) ? calcDepth(pos) : 1.0;
             return;
         }
 
@@ -164,110 +115,158 @@ const fsCode = /* glsl */ `
     }
 `;
 
-const calcHalfSize = (fov, aspect, fovIsHorizontal) => {
-    let x, y;
-    if (fovIsHorizontal) {
-        x = Math.tan(fov * Math.PI / 360);
-        y = x / aspect;
-    } else {
-        y = Math.tan(fov * Math.PI / 360);
-        x = y * aspect;
-    }
-    return [x, y];
-};
-
-const attributes = {
-    vertex_position: SEMANTIC_POSITION
-};
-
-class InfiniteGrid {
-    // shader;
-    // quadRender;
-    // blendState = new BlendState(false);
-    // depthState = new DepthState(FUNC_LESSEQUAL, true);
-
-    visible = true;
+class Grid extends Script {
+    /**
+     * @type {ShaderMaterial}
+     * @private
+     */
+    _material;
 
     /**
-     * @param {AppBase} app - The Application instance
-     * @param {string} layerName - The name of the layer to render into
+     * @type {MeshInstance}
+     * @private
      */
-    constructor(app, layerName = 'World') {
-        this.app = app;
-        this.layerName = layerName;
+    _meshInstance;
 
-        this.device = this.app.graphicsDevice;
-        const shader = createShaderFromCode(this.device, vsCode, fsCode, 'infinite-grid', attributes);
+    /**
+     * @type {Vec2}
+     * @private
+     */
+    _halfExtents = new Vec2();
 
-        this.quadRender = new QuadRender(shader);
-        this.cameraMatrixId = this.device.scope.resolve('camera_matrix');
-        this.cameraParamsId = this.device.scope.resolve('camera_params');
-        this.cameraPositionId = this.device.scope.resolve('camera_position');
-        this.cameraViewProjectionId = this.device.scope.resolve('camera_viewProjection');
+    /**
+     * @type {Color}
+     * @private
+     */
+    _colorX = new Color(1, 0.3, 0.3);
 
-        this.blendState = new BlendState(
-            true,
-            BLENDEQUATION_ADD, BLENDMODE_SRC_ALPHA, BLENDMODE_ONE_MINUS_SRC_ALPHA,
-            BLENDEQUATION_ADD, BLENDMODE_ONE, BLENDMODE_ONE_MINUS_SRC_ALPHA
-        );
-    }
+    /**
+     * @type {Color}
+     * @private
+     */
+    _colorZ = new Color(0.3, 0.3, 1);
 
-    set camera(entity) {
+    constructor(args) {
+        super(args);
+        const {
+            colorX,
+            colorZ
+        } = args.attributes;
 
-        const camera = entity?.camera;
-        if (!entity || !camera || this._camera === entity) return;
+        // ensure the entity has a render component
+        if (!this.entity.render) {
+            this.entity.addComponent('render');
+        }
 
-        this._camera = entity;
-
-        camera.onPreRenderLayer = (layer, transparent) => {
-            if (this.visible && layer.name === this.layerName && !transparent) {
-                this.device.setBlendState(this.blendState);
-                this.device.setCullMode(CULLFACE_NONE);
-                this.device.setDepthState(DepthState.NODEPTH);
-                this.device.setStencilState(null, null);
-
-                // update viewProjectionInverse matrix
-                const cameraMatrix = this._camera.getWorldTransform().clone();
-                const cameraParams = calcHalfSize(camera.fov, camera.aspectRatio, camera.horizontalFov);
-                const cameraPosition = cameraMatrix.getTranslation();
-                const cameraViewProjection = new Mat4().mul2(camera.projectionMatrix, camera.viewMatrix);
-
-                this.cameraMatrixId.setValue(cameraMatrix.data);
-                this.cameraParamsId.setValue(cameraParams);
-                this.cameraPositionId.setValue([cameraPosition.x, cameraPosition.y, cameraPosition.z]);
-                this.cameraViewProjectionId.setValue(cameraViewProjection.data);
-
-                this.quadRender.render();
+        // create shader material
+        this._material = new ShaderMaterial({
+            uniqueName: 'grid-shader',
+            vertexCode,
+            fragmentCode,
+            attributes: {
+                vertex_position: SEMANTIC_POSITION,
+                aUv0: SEMANTIC_TEXCOORD0
             }
-        };
+        });
+        this._material.blendType = BLEND_NORMAL;
+        this._material.cull = CULLFACE_NONE;
+        this._material.update();
+
+        // create mesh
+        const mesh = Mesh.fromGeometry(this.app.graphicsDevice, new PlaneGeometry());
+        this._meshInstance = new MeshInstance(mesh, this._material);
+        this.entity.render.meshInstances = [this._meshInstance];
+
+        // set the initial values
+        this.halfExtents = this._calcHalfExtents(tmpVa) ?? this.halfExtents;
+        this.colorX = colorX ?? this.colorX;
+        this.colorZ = colorZ ?? this.colorZ;
+
+        // update the half extents when the entity scale changes
+        this.app.on('prerender', () => {
+            const halfExtents = this._calcHalfExtents(tmpVa);
+            if (this.halfExtents.distance(halfExtents) > EPISILON) {
+                this.halfExtents = halfExtents;
+            }
+        });
     }
 
-    get camera() {
-        return this._camera;
-    }
-
-    destroy() {
-        const camera = this._camera?.camera;
-        if (camera?.onPreRenderLayer) camera.onPreRenderLayer = null;
-    }
-}
-
-/**
- * The {@link https://api.playcanvas.com/classes/Engine.Script.html | Script} class is
- * the base class for all PlayCanvas scripts. Learn more about writing scripts in the
- * {@link https://developer.playcanvas.com/user-manual/scripting/ | scripting guide}.
- */
-export class Grid extends Script {
     /**
-     * Called when the script is about to run for the first time.
+     * @param {Vec2} vec - The vector to copy the half extents to.
+     * @returns {Vec2} - The half extents.
+     * @private
      */
-    initialize() {
-        this.grid = new InfiniteGrid(this.app);
-        this.on('destroy', _ => this.grid.destroy());
+    _calcHalfExtents(vec) {
+        const scale = this.entity.getLocalScale();
+        return vec.set(scale.x / 2, scale.z / 2);
     }
 
-    update() {
-        const camera = this.app.root.findOne(node => node.camera);
-        this.grid.camera = camera;
+    /**
+     * @param {string} name - The name of the parameter.
+     * @param {Color|Vec2} value - The value of the parameter.
+     * @private
+     */
+    _set(name, value) {
+        if (value instanceof Color) {
+            this._material.setParameter(name, [value.r, value.g, value.b]);
+        }
+
+        if (value instanceof Vec2) {
+            this._material.setParameter(name, [value.x, value.y]);
+        }
+
+        this._material.update();
+
+        this._meshInstance.material = this._material;
+    }
+
+    /**
+     * @attribute
+     * @type {Vec2}
+     */
+    set halfExtents(value) {
+        if (!(value instanceof Vec2)) {
+            return;
+        }
+        this._halfExtents.copy(value);
+        this._set('uHalfExtents', this._halfExtents);
+    }
+
+    get halfExtents() {
+        return this._halfExtents;
+    }
+
+    /**
+     * @attribute
+     * @type {Color}
+     */
+    set colorX(value) {
+        if (!(value instanceof Color)) {
+            return;
+        }
+        this._colorX.copy(value);
+        this._set('uColorX', this._colorX);
+    }
+
+    get colorX() {
+        return this._colorX;
+    }
+
+    /**
+     * @attribute
+     * @type {Color}
+     */
+    set colorZ(value) {
+        if (!(value instanceof Color)) {
+            return;
+        }
+        this._colorZ.copy(value);
+        this._set('uColorZ', this._colorZ);
+    }
+
+    get colorZ() {
+        return this._colorZ;
     }
 }
+
+export { Grid };
