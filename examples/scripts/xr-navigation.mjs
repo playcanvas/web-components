8fdd7f1d20b4ca88f78cf6eddfd46285d2571031
Will Eastcott <will@playcanvas.com>
2024-12-13 16:47:19 +0000

    Lint fixes

--- examples/scripts/xr-navigation.mjs
+++ examples/scripts/xr-navigation.mjs
@@ -1,5 +1,4 @@
-/* eslint-disable-next-line import/no-unresolved */
-import { Color, Script, Vec3 } from 'playcanvas';
+import { Color, Quat, Script, Vec3 } from 'playcanvas';
 
 /** @import { XrInputSource } from 'playcanvas' */
 
